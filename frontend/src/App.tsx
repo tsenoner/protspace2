e65688fe93c5e17e7fd5d788812d8f9c97a5c731
Gökhan Sisman <ge26pec@mytum.de>
2024-03-22 17:14:11 +0100

    Testing colab data receive

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,28 @@
+import { useEffect } from "react";
 import "./App.css";
 import VisualizationComp from "./components/VisualizationComp";
 
 function App() {
+  useEffect(() => {
+    // Function to handle the message event
+    const handleMessage = (event: any) => {
+      // Optional: Check the origin of the message sender for security
+      // if (event.origin !== "http://expected-origin.com")
+      //   return;
+
+      // Assuming your message is a simple object
+      console.log("Message received from colab:", event.data);
+
+      // TODO: Handle the received message as needed
+    };
+
+    // Add the event listener for receiving messages
+    window.addEventListener("message", handleMessage);
+
+    // Cleanup: Remove the event listener when the component unmounts
+    return () => window.removeEventListener("message", handleMessage);
+  }, []); // The empty array ensures this effect runs only once on mount
+
   return (
     <>
       <VisualizationComp />
