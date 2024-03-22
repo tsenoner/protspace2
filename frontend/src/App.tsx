3ad368106047c9f34406e7ec163ec915ad33a80a
Gökhan Sisman <ge26pec@mytum.de>
2024-03-22 18:15:08 +0100

    Clean index html

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,18 +5,6 @@ import VisualizationComp from "./components/VisualizationComp";
 function App() {
   useEffect(() => {
     const handleMessage = (event: { origin: string; data: any }) => {
-      console.log("Received message event:", event);
-
-      // Log the origin of the received message
-      console.log("Message origin:", event.origin);
-
-      // Conditional origin check for security (adjust the origin according to your sending site)
-      if (event.origin !== "https://colab.research.google.com") {
-        console.error("Message from unknown origin:", event.origin);
-        return; // Stop processing the message further if the origin is not recognized
-      }
-
-      // Assuming your message is a simple object or string
       console.log("Message received from Colab:", event.data);
     };
 
@@ -24,7 +12,6 @@ function App() {
     return () => window.removeEventListener("message", handleMessage);
   }, []);
 
-  console.log("App rendered");
   return (
     <>
       <VisualizationComp />
